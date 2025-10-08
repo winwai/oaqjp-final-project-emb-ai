fdf0f467a5cbfa253dda014c9865c9f651c97f38
winwai <winphyowai@gmail.com>
2025-10-08 09:07:15 -0400

    pylint verified

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of emotion
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -7,23 +12,48 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Analyze the emotion of the given text using Watson NLP.
 
+    Args:
+        text (str): The input text to analyze.
+
+    Returns:
+        dict: The emotion result.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
     # Return the result
     if response['dominant_emotion'] is not None:
-        output_string = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
-    else: 
+        output_string = (
+            f"For the given statement, the system response is "
+            f"'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, "
+            f"'fear': {response['fear']}, "
+            f"'joy': {response['joy']} "
+            f"and 'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
+        )
+    else:
         output_string = "Ivalid text! Please try again"
     # Return the formatted string
     return output_string
 
 @app.route("/")
 def render_index_page():
+    """
+    render the page.
 
+    Args:
+        none
+
+    Returns:
+        rendered index.html
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
